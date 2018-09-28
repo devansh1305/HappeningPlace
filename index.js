95b48aa0ac15dc74606a34ae27ac1354156335f0
devansh1305 <dpanirwa@purdue.edu>
2018-09-28 12:43:20 -0400

    redirection changes

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 /* addUser(userName,userPassword)
    Takes two parameters input and stores them in the AWS DynamoDB
    Sample Request: addUser("Viswajeeet Balaji","HappeningPlace Password");
-*/
+ */
 function addUser(userName,userPassword)
 {
-  // Create new XMLHttpRequest. Declare the endpoint and send parameters data in JSON form.
-var req = new XMLHttpRequest();
-req.open('POST','https://9ojvtxgwid.execute-api.us-east-1.amazonaws.com/signupTest/user-signup');
-req.onreadystatechange = function(event)
-{
-  console.log(event.target.response);
-};
-var parameters = {
-  username:userName,
-  password:userPassword,
-}
-req.send(JSON.stringify(parameters));
+	// Create new XMLHttpRequest. Declare the endpoint and send parameters data in JSON form.
+	var req = new XMLHttpRequest();
+	req.open('POST','https://9ojvtxgwid.execute-api.us-east-1.amazonaws.com/signupTest/user-signup');
+	req.onreadystatechange = function(event)
+	{
+		console.log(event.target.response);
+	};
+	var parameters = {
+username:userName,
+	 password:userPassword,
+	}
+	req.send(JSON.stringify(parameters));
 }
 function signup()
 {
-  addUser(document.getElementById("inputEmail4").value, document.getElementById("inputPassword4").value);
+	addUser(document.getElementById("inputEmail4").value, document.getElementById("inputPassword4").value);
+	window.location.replace("login.html");
 	// First parameter is username, last parameter is password
 	// TODO get this from the front-end html using document.getElementByID and call this function
 }
