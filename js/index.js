95d049b49eff8f5acedbff8a40cde6af180eb328
Viswa <balajiv@purdue.edu>
2018-10-02 13:21:27 -0400

    new service functions added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,20 @@
    Takes two parameters input and stores them in the AWS DynamoDB
    Sample Request: addUser("Viswajeeet Balaji","HappeningPlace Password");
  */
+
+ /*
+ TODO: Add all endpoints here
+ */
+ var user_sign_up_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/user-signup";
+ var user_login_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/user-login";
+ var user_password_reset_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/user-password-reset";
+ var host_event_list_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/host-event-list";
+ var host_event_guest_list_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/host-event-guest-list";
+ var host_delete_event_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1";
+ var host_create_event_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1";
+ var guest_remove_event_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1";
+ var guest_join_event_endpoint="https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1";
+
 function addUser(userName,userPassword,firstName,lastName,address_1,address_2,_city,_state,_zipcode)
 {
 	// Create new XMLHttpRequest. Declare the endpoint and send parameters data in JSON form.
@@ -58,7 +72,7 @@ function userLogin(username,password)
     if(event.target.responseText==='true' && this.readyState==4)
       {
 					alert("Successful login");
-					location.href="eventDash.html"
+					location.href="createEvent.html"
 		  }
     else if (this.readyState==4)
       alert("Invalid Credentials");
