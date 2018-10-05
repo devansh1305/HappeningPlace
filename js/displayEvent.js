cf55d2aef802ed22fd956f7decbcf94609e80ffa
Seerat Dheer <sdheer@purdue.edu>
2018-10-04 20:39:17 -0400

    started event list display

---
+++ js/displayEvent.js
@@ -0,0 +1,9 @@
+function ListEvent(document.getElementById('username').value){
+    var req = new XMLHttpRequest();
+	req.open('POST',);
+	req.onreadystatechange = function(event);
+
+    for(var i=0; i< array.length; i++){
+        console.log()
+    }
+}
