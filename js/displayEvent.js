5d0682ae2020fa3342ce3214a4bf15d4dd81a6e6
Sreekara <syachama@purdue.edu>
2018-10-04 22:44:06 -0400

    Revert "MAin tests" This reverts commit 3c60239da17c045351501ba17c252f6419934198.

--- js/displayEvent.js
+++ js/displayEvent.js
@@ -1,19 +1,9 @@
-function ListEvent(zipcode) {
-  var req = new XMLHttpRequest();
-  req.open(
-    "POST",
-    "https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/user-event-list"
-  );
-  req.onreadystatechange = function(event) {
-    console.log(event.target.response);
-  };
-  var params = {
-    zip_code: zipcode
-  };
-  req.send(JSON.stringify(params));
-}
+function ListEvent(document.getElementById('username').value){
+    var req = new XMLHttpRequest();
+	req.open('POST',);
+	req.onreadystatechange = function(event);
 
-function ShowList() {
-  console.log("PART1");
-  ListEvent(document.getElementById("zip").value);
+    for(var i=0; i< array.length; i++){
+        console.log()
+    }
 }
