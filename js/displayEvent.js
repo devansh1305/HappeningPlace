8b020f644a884abc43ddc45f44044642d6add69f
Seerat Dheer <sdheer@purdue.edu>
2018-10-14 18:43:42 -0400

    created js function for displaying past events

--- js/displayEvent.js
+++ js/displayEvent.js
@@ -1,9 +1,37 @@
-function ListEvent(document.getElementById('username').value){
-    var req = new XMLHttpRequest();
-	req.open('POST',);
-	req.onreadystatechange = function(event);
+function ListEvent(zipcode) {
+  var req = new XMLHttpRequest();
+  req.open(
+    "POST",
+    "https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/user-event-list"
+  );
+  req.onreadystatechange = function(event) {
+    console.log(event.target.response);
+  };
+  var params = {
+    zip_code: zipcode
+  };
+  req.send(JSON.stringify(params));
+}
 
-    for(var i=0; i< array.length; i++){
-        console.log()
-    }
+function ListPastEvent(UserName) {
+  var req = new XMLHttpRequest();
+  req.open(
+    "POST",
+    "https://md1q5ktq6e.execute-api.us-east-1.amazonaws.com/hp1/user-event-list"
+  );
+  req.onreadystatechange = function(event) {
+    console.log(event.target.response);
+  };
+  var params = {
+    user_name: UserName
+  };
+  req.send(JSON.stringify(params));
+}
+function ShowList() {
+  console.log("PART000");
+  //for (var i = 0; i < event.target.response.length; i++) {
+  //var str = JSON.parse();
+  //console.log(str);
+
+  ListEvent(document.getElementById("zip").value);
 }
