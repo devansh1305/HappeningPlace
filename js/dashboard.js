208dcac0a99daf2a09c8fd06e24d217fafa48df2
Viswa <balajiv@purdue.edu>
2018-10-21 16:55:39 -0400

    new task UI

--- js/dashboard.js
+++ js/dashboard.js
@@ -16,9 +16,8 @@ var event_contributor_list_endpoint = "https://md1q5ktq6e.execute-api.us-east-1.
 
 
 function eventmanager() {
-
   document.getElementById("createEvent").innerHTML = "<div class=\"w3-display-container w3-panel w3-theme-d3\" style=\"padding:0px;\">";
-  document.getElementById("createEvent").innerHTML += "<input class=\"w3-input\" type=\"text\" placeholder=\"Event Name\" id=\"eventname\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Date\" id=\"enterdate\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Time\" id=\"entertime\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Venue\" id=\"entervenue\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter zipcode\" id=\"enterzip\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Event Description\" id=\"description\"><br>";
+  document.getElementById("createEvent").innerHTML += "<input class=\"w3-input\" type=\"text\" placeholder=\"Event Name\" id=\"eventname\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Date\" id=\"enterdate\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Time\" id=\"entertime\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Venue\" id=\"entervenue\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter zipcode\" id=\"enterzip\"><br><input class=\"w3-input\"type=\"text\" placeholder=\"Event Description\" id=\"description\"><br>";
   document.getElementById("createEvent").innerHTML += " <input class=\"w3-input\" type=\"text\" placeholder=\"Enter Tags\" id=\"tags\"><br><button type=\"button\" class=\"w3-button w3-theme-d1\" onclick=\"create()\">Create Event</button>&nbsp<button type=\"button\" class=\"w3-button w3-theme-d1\" onclick=\"cancel()\">Cancel</button></div>";
 }
 
@@ -50,15 +49,6 @@ function openNav() {
 
 
 
-function loadNearEventList() {
-  var near_event_list;
-  text = "";
-  for (i = 0; i < 3; i++) {
-    text += "<div class=\"w3-panel w3-theme-d4 w3-display-container w3-card\"><span onclick=\"this.parentElement.style.display='none'\"class=\"w3-button w3-theme-d1 w3-large w3-display-topright\">&times;</span><h3>Event Name</h3><p>Partytime</p></div>";
-  }
-  document.getElementById("nearEventList").innerHTML = text;
-
-}
 
 
 function hostguestlist() {
@@ -72,24 +62,21 @@ function hostguestlist() {
 
 
 
-function displayHostEventDetails(name) {
+function displayHostEventDetails(currentEvent) {
 
-  var text = "<div class=\"w3-card w3-theme-d4\" style=\"padding-left:5%\"><h2>" + name + "</h2></div>";
-
-  text = text + "<div class=\"w3-bar-item w3-hover-white w3-button w3-card-4 w3-medium w3-theme-d4\" title=\"Remove Event\"><i class=\"fa fa-close\"></i></div>";
-  var text2 = "";
-  var text4 = "";
-
+  var text = "";
   for (var i = 0; i < 3; i++) {
-    text2 += " <button class=\"w3-bar-item w3-hover-white w3-button w3-card-4 w3-medium w3-theme-d2\" onclick=\"displayTaskDetails('" + name + "');\" >" + name + "Task</button>";
-    text4 += " <div class=\"w3-bar-item w3-hover-white w3-button w3-card-4 w3-medium w3-theme-d2\">" + name + "Name</div>";
+    text += "<button class=\"w3-bar-item w3-hover-white w3-button w3-card-4 w3-medium w3-theme-d2\" onclick=\"displayTaskDetails('" + currentEvent + "');\" >" + currentEvent + "Task</button>  ";
   }
-
-  var text3 = "<div class=\"w3-card w3-theme-d4\" style=\"padding-left:5%\"><h2>Contributors</h2></div>";
-
-  document.getElementById("eventDetails").innerHTML = text + text2 + text3 + text4;
+  document.getElementById("tasks").innerHTML = text;
 
 
+}
+
+function addTask() {
+  document.getElementById("createEvent").innerHTML = "<div class=\"w3-display-container w3-panel w3-theme-d3\" style=\"padding:0px;\">";
+  document.getElementById("createEvent").innerHTML += "<input class=\"w3-input\" type=\"text\" placeholder=\"Task Name\" id=\"eventname\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Date\" id=\"enterdate\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Time\" id=\"entertime\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter Venue\" id=\"entervenue\"><br><input class=\"w3-input\" type=\"text\" placeholder=\"Enter zipcode\" id=\"enterzip\"><br><input class=\"w3-input\"type=\"text\" placeholder=\"Event Description\" id=\"description\"><br>";
+  document.getElementById("createEvent").innerHTML += " <input class=\"w3-input\" type=\"text\" placeholder=\"Enter Tags\" id=\"tags\"><br><button type=\"button\" class=\"w3-button w3-theme-d1\" onclick=\"create()\">Create Task</button>&nbsp<button type=\"button\" class=\"w3-button w3-theme-d1\" onclick=\"cancel()\">Cancel</button></div>";
 }
 
 function loadHostEventList(arr) {
@@ -109,14 +96,12 @@ function loadHostEventList(arr) {
 }
 
 function displayTaskDetails(eventID) {
-  
   var text = "";
   text = text + "<div class=\"w3-bar-item w3-hover-white w3-button w3-card-4 w3-medium w3-theme-d4\" title=\"Add Contributer\"><i class=\"fa fa-user-plus\"></i></div>";
   text = text + "<div class=\"w3-bar-item w3-hover-white w3-right w3-button w3-card-2 w3-medium w3-theme-d4\" title=\"Remove Task\"><i class=\"fa fa-close\"></i></div><br>";
   text += "<h2>" + eventID + "</h2><div class=\"w3-theme-d2 w3-card-2\" style=\"padding:10px\"><h4>Task Description"
   text += "</h4><div class=\" w3-bar\"> ";
   text += "<input class=\"w3-check\" type=\"checkbox\"><label>sub_task</label><br><input class=\"w3-small w3-check\" type=\"checkbox\"><label>sub_task</label><br><input class=\"w3-check\" type=\"checkbox\"><label>sub_task</label><br> </div>";
-
   document.getElementById("middleDetailSpace").innerHTML = text;
 }
 
@@ -125,7 +110,6 @@ function retrieveHostEventList() {
   req.open('POST', host_event_list_endpoint);
   req.onreadystatechange = function(event) {
     if (this.readyState == 4) {
-      console.log(JSON.parse(event.target.response));
       loadHostEventList(JSON.parse(event.target.response));
     }
   };
