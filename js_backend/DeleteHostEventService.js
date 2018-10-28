74e4eaa3de5f48a3736c386fb576fc4b0f356080
Devansh Panirwala <dpanirwa@purdue.edu>
2018-10-28 19:21:54 -0400

    added lambda function to remove guest

--- js_backend/DeleteHostEventService.js
+++ js_backend/DeleteHostEventService.js
@@ -21,6 +21,42 @@ exports.handler = (event, context, callback) => {
         {
             //callback(null,data1.Item.guest_list.SS);
             //console.log(data1.Item.contributors.SS);
+            (data1.Item.contributors.SS).forEach(function(_element){
+                var aws=require('aws-sdk');
+                var lambda=new aws.Lambda({
+                    region: 'us-east-1'
+                });
+
+                var temp_param={
+                    username: _element,
+                    eventID:  data1.Item.eventID.S
+                };
+                var params2={
+                    FunctionName: 'removeEventContributorsService',
+                    Payload: JSON.stringify(temp_param,null,2)
+                };
+                
+                lambda.invoke(params2,function(err1,data2){
+                   
+                    if(err1){
+                        
+                        context.done('error_invoke_error_contributor',err1);
+                    }
+                    else if(data2.Payload){
+                        //console.log(data2.Payload);
+                        context.succeed(data2.Payload);
+                    }
+                    /*
+                    else
+                    {
+                        callback("Success in invoking");
+                    }
+                    */
+                });
+                //console.log("Test_12_3");
+                //console.log(_element);
+            });
+            
             (data1.Item.guest_list.SS).forEach(function(_element){
                 var aws=require('aws-sdk');
                 var lambda=new aws.Lambda({
@@ -40,7 +76,7 @@ exports.handler = (event, context, callback) => {
                    
                     if(err1){
                         
-                        context.done('error_invoke_error',err1);
+                        context.done('error_invoke_error_guest',err1);
                     }
                     else if(data2.Payload){
                         //console.log(data2.Payload);
