4f9416f892296d0595a17a53a6a69cc816a3011d
Viswa <balajiv@purdue.edu>
2018-11-18 18:22:22 -0500

    profile connection

---
+++ js_backend/profile.js
@@ -0,0 +1,54 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({region:'us-east-1',apiVersion:'2012-08-10'});
+
+exports.handler = (event, context, callback) => {
+    // TODO implement
+
+    var params = {
+        TableName:'User',
+        Key:{
+            username: {
+                S: event.username
+            }
+        },
+        UpdateExpression: "set profileAccess = :r",
+        ExpressionAttributeValues:{
+            ":r":{
+                S: event.profileAccess
+            }
+        },
+        ReturnValues:"UPDATED_NEW"
+    };
+
+
+    var params1={
+        TableName:'User',
+        Key: {
+            username: {
+                S: event.username
+            }
+        }
+    };
+
+    dynamodb.getItem(params1,function(err,data){
+        if (err)
+        {
+            callback(err);
+        }
+        else
+        {
+            dynamodb.updateItem(params, function(err, data) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    callback(null,true);
+                }
+                });
+
+
+        }
+    });
+
+
+};
